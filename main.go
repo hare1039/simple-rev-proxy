695f76346665260b91e86b0188c6aedfb7c138ab
hare1039 <hare39393@gmail.com>
2017-09-13 21:23:13 +0800

    Change listen port to manual set

--- main.go
+++ main.go
@@ -9,12 +9,12 @@ import (
 
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Println("Please specify `simple-rev-tunnel server` or `simple-rev-tunnel client`")
+		fmt.Println("Please specify `simple-rev-tunnel server [:port]` or `simple-rev-tunnel client [ip:port]`")
 		os.Exit(1)
 	}
 	if os.Args[1] == "server" || os.Args[1] == "s" {
-		server.Start(":1256")
+		server.Start(os.Args[2])
 	} else if os.Args[1] == "client" || os.Args[1] == "c" {
-		client.Connect("localhost:1256")
+		client.Connect(os.Args[2])
 	}
 }
